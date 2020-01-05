28ee52c02906117afd3bcf4a554ff6a84103e75d
Gael James <gaeljames@gmail.com>
2020-01-05 18:56:12 +0100

    Fix web page

--- plugin.py
+++ plugin.py
@@ -17,45 +17,15 @@ For more details, see [Using Python Plugins](https://www.domoticz.com/wiki/Using
 
 """
 <plugin
-    key="GaelJDomoticzAdvancedThermostat"
-    name="GDAT"
+    key="Chacon433"
+    name="Chacon433"
     author="gaelj"
     version="1.0.0"
-    wikilink="https://github.com/gaelj/DomoticzAdvancedThermostatPlugin/blob/master/README.md"
-    externallink="https://github.com/gaelj/DomoticzAdvancedThermostatPlugin">
+    wikilink="https://github.com/gaelj/Chacon433Plugin//blob/master/README.md"
+    externallink="https://github.com/gaelj/Chacon433Plugin">
 
     <description>
-        <h2>Domoticz Advanced Thermostat</h2><br/>
-        This thermostat plugin allows precise temperature management and regulation per individual room
-
-        <h3>Modes</h3>
-        <ul style="list-style-type:square">
-            <li>off</li>
-            <li>away</li>
-            <li>night</li>
-            <li>eco</li>
-            <li>Normal</li>
-            <li>comfort</li>
-        </ul>
-
-        <h3>Features</h3>
-        <ul style="list-style-type:square">
-            <li>individual room temperature management</li>
-            <li>global presence management</li>
-            <li>room presence management</li>
-            <li>control thermostat radiator valves temperature settings</li>
-            <li>read thermostat radiator valves temperature measurements</li>
-        </ul>
-
-        <h3>Devices</h3>
-        <ul style="list-style-type:square">
-            <li>Thermostat Control: Off|Away|Night|Normal|Comfort</li>
-            <li>Room 1 Presence: Absent|Present</li>
-            <li>Room 2 Presence: Absent|Present</li>
-        </ul>
-
-        <h3>Configuration</h3>
-        Configuration options...
+        <h2>Domoticz Chacon 433 interface</h2><br/>
     </description>
 
     <params>
@@ -63,13 +33,8 @@ For more details, see [Using Python Plugins](https://www.domoticz.com/wiki/Using
         <param field="Port"     label="Port"                                                         width="40px"  required="true"  default="8080"      />
         <param field="Username" label="Username"                                                     width="200px" required="false" default=""          />
         <param field="Password" label="Password"                                                     width="200px" required="false" default=""          />
-        <param field="Mode1" label="Apply minimum heating per cycle" width="200px">
-            <options>
-				<option label="ony when heating required" value="Normal"  default="true" />
-                <option label="always" value="Forced"/>
-            </options>
-        </param>
-        <param field="Mode2" label="Calculation cycle, Minimum Heating time per cycle, Pause On delay, Pause Off delay, Forced mode duration (all in minutes)" width="200px" required="true" default="30,0,2,1,60"/>
+        <param field="Mode1" label="DIOShutterCode" width="200px" required="true" />
+        <param field="Mode2" label="Shutter index" width="200px" required="true" default="0"/>
         <param field="Mode3" label="Logging Level" width="200px">
             <options>
                 <option label="Normal" value="Normal" default="true"/>
@@ -85,11 +50,6 @@ For more details, see [Using Python Plugins](https://www.domoticz.com/wiki/Using
     </params>
 </plugin>
 """
-
-# <param field="Mode1"    label="Inside Temperature Sensors, grouped by room (idx1, idx2...)"  width="300px" required="true"  default=""          />
-# <param field="Mode2"    label="Outside Temperature Sensors (idx1, idx2...)"                  width="300px" required="true"  default=""          />
-# <param field="Mode3"    label="Heating switch + Inside Radiator Setpoints, grouped by room (idx1, idx2...)"   width="300px" required="true"  default=""          />
-
 
 import Domoticz
 from datetime import datetime, timedelta
