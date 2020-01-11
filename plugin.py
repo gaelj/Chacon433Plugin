eeef85f070103452120032bf7cf86204aa6b4cec
Gaël James <gaeljames@gmail.com>
2020-01-11 18:19:26 +0100

    Bugfix

--- plugin.py
+++ plugin.py
@@ -79,7 +79,7 @@ class PluginConfig:
 class PluginDevices:
     def __init__(self, shutterIds):
         self.config = PluginConfig()
-        self.shutters = dict([(i, ShutterActuator(i, x))
+        self.shutters = dict([(int(x), ShutterActuator(i, x))
                               for i, x in enumerate(shutterIds)])
 
 
